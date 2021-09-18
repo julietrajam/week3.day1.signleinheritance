bd88fb8b67e70b15f048684da7cc805fa432ef91
julietrajam <44438970+julietrajam@users.noreply.github.com>
2021-09-18 23:15:14 +0530

    Add files via upload

---
+++ Desktop.java
@@ -0,0 +1,18 @@
+package org.system;
+
+public class Desktop extends Computer {
+
+	public void desktopSize() {
+		System.out.println("Desktop Size : 18 inches");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Desktop d1 = new Desktop();
+		d1.computerModel();
+		d1.desktopSize();
+
+	}
+
+}
